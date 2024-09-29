40ecc845942ae67fad8c4b93ccb49a3374872001
peb7268 <peb7268@gmail.com>
2024-09-29 10:57:11 -0600

    wip

--- excercise_server.py
+++ excercise_server.py
@@ -2,19 +2,79 @@
 
 import flask
 from flask import request, jsonify
+from bs4 import BeautifulSoup
+
 import os
+import json
+
+import requests
+import urllib.parse
 
 from exercise_picker_cli import diastasis_recti_exercises, pick_random_exercise
 
+# make a constant for the youtube api key that is pulled from the environment or falls back to a default value
+YOUTUBE_API_KEY = os.getenv('YOUTUBE_API_KEY', 'AIzaSyD-9tSr1k1vq1ZgK0CJhj6qKf6Qp4i4KdI')
+YOUTUBE_EMBED_URL = "https://www.youtube.com/embed?listType=search&list="
+
 
 app = flask.Flask(__name__)
-
 
 
 def send_from_directory(directory, filename):
     print(directory)
     print(filename)
     return flask.send_from_directory(directory, filename)
+
+# Query youtube for a video about the excercise.name and add the video embed link to the excercise object
+def decorate_exercises(exercises):
+    # print(f"excercises to decorate: {json.dumps(exercises, indent=4)}")
+
+    for exercise_level in exercises:
+        # print(f"excercise name: {json.dumps(exercises[exercise_level], indent=4)}")
+        # print(f"exercise name: {exercises[exercise_level]}")
+
+        for exercise in exercises[exercise_level]:
+            exercise_name = exercise['name']
+            print(f"excercise name: {exercise_name}")
+
+            # query the youtube api embed URL for a video about the exercise
+            query = exercise['name'] + " exercise"
+            query = urllib.parse.quote(exercise_name)
+            print(f"query: {query}")
+            response = requests.get(f"https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q={query}&key={YOUTUBE_API_KEY}")
+            print(f"response: {response}")
+
+            # parse the response to get the video link
+            video_link = None
+            if response.status_code == 200:
+                response_json = response.json()
+                video_id = response_json['items'][0]['id']['videoId']
+                video_link = f"{YOUTUBE_EMBED_URL}{video_id}"
+                print(f"video link: {video_link}")
+            else:
+                print(f"Error: {response.status_code}")
+
+        
+        
+        # add the video link to the exercise object
+        exercise['video'] = video_link
+
+
+
+    return exercises
+
+        # Example request:
+
+        
+
+
+
+        
+
+
+        # https://www.youtube.com/embed?listType=search&list=diastasis+recti+exercise
+        
+    return
 
 @app.route('/info', methods=['GET'])
 def info():
@@ -52,9 +112,15 @@ def get_exercise():
     l3 = request.args.get('l3', 'Level 3')
 
 
-    # pick_random_exercise(diastasis_recti_exercises, args.L1, args.L2, args.L3)
-    exercise = pick_random_exercise(diastasis_recti_exercises, l1, l2, l3)
-    return jsonify(exercise)
+    # resolveExcerciseList()
+    exercises = pick_random_exercise(diastasis_recti_exercises, l1, l2, l3)
+
+    decorated_excercises = decorate_exercises(exercises)
+
+    #pretty print the exercises in json format
+    print(json.dumps(decorated_excercises, indent=4))
+
+    return jsonify(decorated_excercises)
 
 
 # Run the server
